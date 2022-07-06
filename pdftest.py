15cf6cf8cb43ce95d57a0eceb86fbad69eb3dfd1
Cris <68723063+CrxsTagz@users.noreply.github.com>
2022-07-05 22:06:15 -0400

    added

--- pdftest.py
+++ pdftest.py
@@ -3,22 +3,22 @@ from fpdf import FPDF
 
 #PDF function
 
-def getpdf():
-    wpascore = random()
-    fwfhs = random()
-    #save FPDF class into a variable pdf
-    pdf = FPDF()
+
+wpascore = random()
+fwfhs = random()
+#save FPDF class into a variable pdf
+pdf = FPDF()
 
 #Add page
-    pdf.add_page()
+pdf.add_page()
 
-    pdf.set_font("Arial", size= 13)
+pdf.set_font("Arial", size= 13)
 
 #create cells 
 
-    pdf.cell(200, 10,  txt= f"Tu score en wpa es: {wpascore}", ln= 1, align= 'L')
-    pdf.cell(200, 10,  txt= f"Tu score en wpa es: {fwfhs}", ln= 2, align= 'L')
+pdf.cell(200, 10,  txt= f"Tu score en wpa es: {wpascore}", ln= 1, align= 'L')
+pdf.cell(200, 10,  txt= f"Tu score en wpa es: {fwfhs}", ln= 2, align= 'L')
 
 #save the pdf with the .pdf extension
 
-    pdf.output("Prueba.pdf")
+pdf.output("test.pdf") 
