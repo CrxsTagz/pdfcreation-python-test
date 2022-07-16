92cc42afb1382da3aa3314a53a46023afe76f247
CrxsTagz <mynameiscris000@gmail.com>
2022-07-16 19:05:51 -0400

    added code in line 141

--- PDF_creation.py
+++ PDF_creation.py
@@ -6,8 +6,8 @@ from datetime import datetime
 # This function does not have a return, it only generates the PDF assesment.
 #customerName = 'PUCMM'
 
-def pdfGenerator(customerName):
-    # WSSid = int(WSSid)
+def pdfGenerator(WSSid, customerName):
+    WSSid = int(WSSid)
     customerName = customerName
     # save FPDF class into a variable pdf
     pdf = FPDF('P', 'mm', (300, 150))
@@ -42,8 +42,9 @@ def pdfGenerator(customerName):
                                              database='sifi',
                                              user='root',
                                              password='sifi')
-            sql_select_Query = "select * from wss"
             cursor = connection.cursor()
+            sql_select_Query = "SELECT * FROM wss WHERE wssID ="+str(WSSid)+";"
+            
             cursor.execute(sql_select_Query)
         # get all records
             records = cursor.fetchall()
@@ -58,7 +59,7 @@ def pdfGenerator(customerName):
                 print("handshake  = ", row[4])
                 print("cracked_password  = ", row[5])
                 print("test_type  = ", row[6], "\n")
-                # print("Customer Name  = ", row[7], "\n")
+                print("Customer Name  = ", row[7], "\n")
                 if row[4] is not None:
                     print("Se ha capturado el handshake")
 
@@ -66,6 +67,7 @@ def pdfGenerator(customerName):
             print("Error while connecting to MySQL", e)
     finally:
             if connection.is_connected():
+                cursor.close()
                 connection.close()
             print("MySQL connection is closed")
 
@@ -79,7 +81,7 @@ def pdfGenerator(customerName):
     pdf.cell(200, 10,  txt= f"El handshake capturado es : {row[4]}", ln= 7, align= 'L')
     pdf.cell(200, 10,  txt= f"Contraseña del AP : {row[5]}", ln= 8, align= 'L')
     pdf.cell(200, 10,  txt= f"Tipo de test realizado: {row[6]}", ln= 9, align= 'L')
-    #pdf.cell(200, 10,  txt= f"Empresa/Cliente: {row[7]}", ln= 10, align= 'L')
+    pdf.cell(200, 10,  txt= f"Empresa/Cliente: {row[7]}", ln= 10, align= 'L')
 
     if row[4] is not None:
         pdf.add_page()
@@ -115,12 +117,12 @@ def pdfGenerator(customerName):
             pdf.cell(200, 10,  txt= "Segun el libro CWSP en su capitulo 9.1.8 se recomienda actualizar a una solución de autenticación 802.1X/EAP usando autenticación ", ln= 14, align= 'L')
             pdf.cell(200, 1,  txt= "tunelada.", ln= 15, align= 'L')
             pdf.cell(200, 10, txt= "A parte de haber capturado el 4-full way handshake, se pudo hacer un crack de la contraseña", ln= 13, align= 'L')
-            pdf.cell(200, 10,  txt= "La universidad de Georgia en su articulo 'Password Policy' recomienda utilizar contraseñas aceptadas en el rango de seguridad. La", ln= 14, align= 'L')
+            pdf.cell(200, 1,  txt= "La universidad de Georgia en su articulo 'Password Policy' recomienda utilizar contraseñas aceptadas en el rango de seguridad. La", ln= 14, align= 'L')
             pdf.cell(200, 1,  txt= " misma debe tener al menos 10 caracteres con una combinacion alfanumerica y caracteres especiales ", ln= 14, align= 'L')
 
             pdf.set_font("Arial", size= 16)
             pdf.cell(110)
-            pdf.cell(200, 10,  txt= "Mejores practicas a tomar", ln= 25, align= 'L')
+            pdf.cell(200, 10,  txt= "Mejores practicas a tomar", ln= 24, align= 'L')
             pdf.set_font("Arial", size= 13)
             pdf.cell(-110)
             pdf.cell(200, 10,  txt= "- Política corporativa: Un apéndice adicional a las recomendaciones de seguridad podrían ser las recomendaciones de políticas WLAN ", ln= 19, align= 'L')
@@ -128,8 +130,7 @@ def pdfGenerator(customerName):
             pdf.ln(2)
             pdf.cell(200, 10,  txt= "- Seguridad física: La instalación de unidades de cerramiento para proteger contra el robo y el acceso físico no autorizado a los puntos", ln= 21, align= 'L')
             pdf.cell(200, 1,  txt= "de acceso puede ser una recomendación. Estas también se utilizan a menudo con fines estéticos. ", ln= 22, align= 'L')
-            
-            pdf.add_page()
+            pdf.page()
             
             # Define margins
             epw = pdf.w - pdf.l_margin - pdf.r_margin
@@ -138,28 +139,27 @@ def pdfGenerator(customerName):
             # Draw new margins.
             pdf.rect(pdf.l_margin, pdf.t_margin, w=epw, h=eph)
 
-            pdf.set_font("Arial", size= 16)
-            pdf.cell(100)
-            pdf.cell(200, 10,  txt= "Directrices de contraseñas PCI-DSS", ln= 21, align= 'L')
-            pdf.set_font("Arial", size= 13)
-            pdf.cell(-100)
-            pdf.cell(200, 10,  txt= "Jithukrishnan plantea en el blog Securden los siguiente pasos para asegurar la proteccion de datos: ", ln= 22, align= 'L')
-            pdf.ln(5)
-            pdf.cell(200, 10, txt = "- Se debe cambiar siempre los valores predeterminados que fueron proporcionados por el proveedor, esto incluye las contraseñas y las ", ln=23, align='L')
-            pdf.cell(200, 1, txt= "configuraciones como tambien deshabilitar las cuentas innecesarias antes de instalar su propio sistema de red.  ", ln= 24, align='L')
-            pdf.ln(2)
-            pdf.cell(200, 10, txt= "- Eliminar o deshabilitar cuentas de usuarios inactivas dentro de 90 dias", ln= 25, align='L')
-            pdf.cell(200, 10, txt= "- Limitar los intentos de acceso repetidos bloqueando el ID de usuario despues de mas de 5 intentos y establecer una duracion de", ln= 26, align='L')
-            pdf.cell(200, 1, txt= "bloqueo en un minimo de 30 minutos o hasta que el administrador habilite el ID del usuario.", ln=27, align='L')
-            pdf.ln(2)
-            pdf.cell(200, 10, txt= "- Se debe cambiar la contraseña de los usuarios una vez cada 90 dias y no permitir que se repita las ultimas cuatro contraseñas.", ln= 28, align='L')
-            pdf.ln(2)
-            pdf.cell(200, 10, txt= "- Establecer contraseñas para el primer uso y al restablecerlas a un valor unico para cada usuario y cambiar inmediatamente despues", ln= 29, align='L')
-            pdf.cell(200, 1, txt= " del primer uso. Hacer cumplir la autenticacion multifactor ", ln= 30, align='L')
+            chars = set('!#$%&()*+,-./:;<=>?@[\]^_`{|}~')
+
+            if any((c in chars) for c in row[5]):
+                pdf.set_font("Arial", size= 16)
+                pdf.cell(110)
+                pdf.cell(200, 10,  txt= "Directrices de contraseñas PCI-DSS", ln= 21, align= 'L')
+                pdf.set_font("Arial", size= 13)
+                pdf.cell(-110)
+                pdf.cell(200, 10,  txt= "Jithukrishnan plantea en el blog Securden los siguiente pasos para asegurar la proteccion de datos: ", ln= 21, align= 'L')
+                pdf.cell(200, 10, txt = "- Se debe cambiar siempre los valores predeterminados que fueron proporcionados por el proveedor, esto incluye las contraseñas y las ", ln=25, align='L')
+                pdf.cell(200, 1, txt= "configuraciones como tambien deshabilitar las cuentas innecesarias antes de instalar su propio sistema de red.  ", ln= 25, align='L')
+                pdf.cell(200, 10, txt= "- Eliminar o deshabilitar cuentas de usuarios inactivas dentro de 90 dias", ln= 25, align='L')
+                pdf.cell(200, 10, txt= "- Limitar los intentos de acceso repetidos bloqueando el ID de usuario despues de mas de 5 intentos y establecer una duracion de", ln= 25, align='L')
+                pdf.cell(200, 1, txt= "bloqueo en un minimo de 30 minutos o hasta que el administrador habilite el ID del usuario.", ln=25, align='L')
+                pdf.cell(200, 10, txt= "- Se debe cambiar la contraseña de los usuarios una vez cada 90 dias y no permitir que se repita las ultimas cuatro contraseñas.", ln= 25, align='L')
+                pdf.cell(200, 10, txt= "- Establecer contraseñas para el primer uso y al restablecerlas a un valor unico para cada usuario y cambiar inmediatamente despues del ", ln= 25, align='L')
+                pdf.cell(200, 1, txt= " primer uso. Hacer cumplir la autenticacion multifactor ", ln= 25, align='L')
 
         #Vulnerable	2 pentesting con resultados negativos de cualquier función de la plataforma.
 
-    elif row[4] is not None and row[5] is None:
+    elif row[4] != "" and row[5] is None:
             pdf.set_font("Arial", size= 16)
             pdf.cell(125)
             pdf.cell(200, 10,  txt= "Sifi Score: ", ln= 24, align= 'L')
@@ -188,7 +188,7 @@ def pdfGenerator(customerName):
             pdf.cell(200, 10,  txt= "- Seguridad física: La instalación de unidades de cerramiento para proteger contra el robo y el acceso físico no autorizado a los puntos", ln= 21, align= 'L')
             pdf.cell(200, 1,  txt= "de acceso puede ser una recomendación. Estas también se utilizan a menudo con fines estéticos. ", ln= 22, align= 'L')
 
-    elif row[4] is None and row[5] is None:
+    elif row[4] == "" and row[5] is None:
         pdf.set_font("Arial", size= 16)
         pdf.cell(125)
         pdf.cell(200, 10,  txt= "Sifi Score: ", ln= 24, align= 'L')
@@ -200,6 +200,4 @@ def pdfGenerator(customerName):
         # pdf.cell(200, 10, txt= "No se ha capturado el handshake", ln= 11, align= 'L')
 
     #save the pdf with the .pdf extension
-    pdf.output("SiFi_{}_{}_{}.pdf".format(Assesment_ID, customerName, date))
-    
-pdfGenerator("PUCMM")
+    pdf.output("PDFReports/SiFi_{}_{}_{}.pdf".format(Assesment_ID, customerName, date))
